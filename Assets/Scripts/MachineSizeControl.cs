bbe21435ed7ea1e59cf01dd0ac0946a3dd910ba5
Bhakti Utama Ganda Pamungkas <bhakti.gamelevelone@gmail.com>
2018-05-07 10:46:41 +0700

    Arcade Room + New Scene "MainBhakti" and Canvas UI Prefab

--- Assets/Scripts/MachineSizeControl.cs
+++ Assets/Scripts/MachineSizeControl.cs
@@ -10,13 +10,15 @@ public class MachineSizeControl : MonoBehaviour {
 	public Slider sizeSlider;
 
 	void Start() {
-		sizeSlider.value = 1f;
+        sizeSlider.maxValue = 0.2f;
+		sizeSlider.value = 0.1f;
 		SliderChange ();
 	}
 
 	public void SliderChange() {
 		Vector3 newSize = new Vector3 (sizeSlider.value,sizeSlider.value,sizeSlider.value);
 //		sizeText.text = sizeSlider.value.ToString ("N2");
-		machine.localScale = newSize;
+        //Vector3 machineSize = new Vector3(machine.localScale.x * newSize.x, machine.localScale.y * newSize.y, machine.localScale.z * newSize.z);
+        machine.localScale = newSize;
 	}
 }
